fca6c065d6254c53098858bebf286527b66d11af
Charlie Cain <ccain21@students.psc.ac.uk>
2022-11-30 16:13:41 +0000

    lists of list for weeekly report

--- Globe Trotter project/weekly report.cs
+++ Globe Trotter project/weekly report.cs
@@ -36,30 +36,39 @@ namespace Globe_Trotter_project
             string _sSqlString;
             double unitdistance;
             TimeSpan dt;
-            List<string> tabledata = new List<string>();
+            List<List<string>> tabledata = new List<List<string>>();
+
+
             userIDlb.Text = _logid;
+
 
             _sSqlString = "SELECT JourneyID, DateofJourney, StartLocalID, EndLocalID, Distance, StartTime, EndTime " +
                 "FROM Journey WHERE  EmployeeID =" + _logid + " ORDER BY DateofJourney DESC";
-            tabledata = ReadSql(_sSqlString);
-            tabledata[1] = tabledata[1].Substring(0,10);
+            tabledata = ReadSqls(_sSqlString);
 
-            _sSqlString = "SELECT LocationName FROM Location WHERE LocationID = " + tabledata[2];
-            tabledata[2] = database.ReadSql(_sSqlString);
+            for (int i = 0; i < tabledata.Count; i++)
+            {
+                List<string> tablerecord = tabledata[i];
 
-            _sSqlString = "SELECT LocationName FROM Location WHERE LocationID = " + tabledata[3];
-            tabledata[3] = database.ReadSql(_sSqlString);
 
-            unitdistance = Convert.ToDouble(tabledata[4]) * 1.852;
-            unitdistance = Math.Round(unitdistance, 2);
-            tabledata[4] = unitdistance.ToString();
-            tabledata[4] = unitdistance + " km";
+                tablerecord[1] = tablerecord[1].Substring(0, 10);
 
-            dt = Convert.ToDateTime(tabledata[6]) - Convert.ToDateTime(tabledata[5]);
-            tabledata[5] = dt.ToString();  
-            
-            reporttbl.Rows.Add(new object[] { tabledata[0], tabledata[1], tabledata[2], tabledata[3], tabledata[4], tabledata[5] });
-            
+                _sSqlString = "SELECT LocationName FROM Location WHERE LocationID = " + tablerecord[2];
+                tablerecord[2] = database.ReadSql(_sSqlString);
+
+                _sSqlString = "SELECT LocationName FROM Location WHERE LocationID = " + tablerecord[3];
+                tablerecord[3] = database.ReadSql(_sSqlString);
+
+                unitdistance = Convert.ToDouble(tablerecord[4]) * 1.852;
+                unitdistance = Math.Round(unitdistance, 2);
+                tablerecord[4] = unitdistance.ToString();
+                tablerecord[4] = unitdistance + " km";
+
+                dt = Convert.ToDateTime(tablerecord[6]) - Convert.ToDateTime(tablerecord[5]);
+                tablerecord[5] = dt.ToString();
+
+                reporttbl.Rows.Add(new object[] { tablerecord[0], tablerecord[1], tablerecord[2], tablerecord[3], tablerecord[4], tablerecord[5] });
+            }
 
         }
 
@@ -70,7 +79,7 @@ namespace Globe_Trotter_project
 
         private const string EXAMPLEDB = "ExampleDatabase.mdb";
         private const string CONNECTION_STRING = @"Provider=Microsoft Jet 4.0 OLE DB Provider;Data Source = " + EXAMPLEDB + ";";
-        public static List<string> ReadSql(String sSqlString)
+        public static List<List<string>> ReadSqls(String sSqlString)
         {
             OleDbDataReader reader = null;
             using (OleDbConnection connection = new OleDbConnection(CONNECTION_STRING))
@@ -83,34 +92,24 @@ namespace Globe_Trotter_project
                         Console.WriteLine(sSqlString);
                         connection.Open();
                         reader = command.ExecuteReader();
-                        if (reader.Read())
+                        List<List<string>> Results = new List<List<string>>();
+                        while (reader.Read())
                         {
-                            string readed;
-                            
-                            List<string> SQl = new List<string>(10);
-                            
+                            List<string> SQl = new List<string>();
                             for (int i = 0; i < 7; i++)
                             {
-                                readed = reader.GetValue(i).ToString();
-                                SQl.Add(readed);
+                               
+                                SQl.Add(reader.GetValue(i).ToString());
                             }
-                            return SQl;
-                           
+                            Results.Add(SQl);
+                   
                         }
-                        else
-                        {
-                            return null;
-                        }
-
-
-
+                        return Results;                  
                     }
                     catch (Exception ex)
                     {
-                        List<string> SQl = new List<string>();
-                        
-                        SQl.Add("error1");
-                        return SQl;
+                        List<List<string>> Results = new List<List<string>>();
+                        return Results;
                     }
                 }
             }
